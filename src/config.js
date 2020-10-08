470e9dacfeeec1945ed28acabb8d6b53bf4e7188
Jany Battung <janybattung@gmail.com>
2020-10-08 15:29:42 -0700

    update files

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$D54CYxChzIDaTP9lWC.2LeDeoqs8it4NM5aNBAFvYuA80M491Ghw2',
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
